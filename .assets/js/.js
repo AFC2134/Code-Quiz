cbc3f2f9b027240affdacf42918c2623a7ae77e0
afc2134 <85919290+AFC2134@users.noreply.github.com>
2021-08-01 03:25:53 -0500

    paths-html-javaframe-queryselectors

---
+++ .assets/js/.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+var buttonEl = document.querySelector("#begin-quiz");
+
+
+
+buttonEl.addEventListener("click", function() {
+    alert("You Have one minute to answer as many questions as possible!");
+  });
